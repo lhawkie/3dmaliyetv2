e50194581543c0f2adce1cacff3ed5bf778a4232
lhawkie <sevkethandanaci@gmail.com>
2025-05-13 17:06:31 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PrintCostCalculator from './print-cost'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PrintCostCalculator />
+  </React.StrictMode>
+)
